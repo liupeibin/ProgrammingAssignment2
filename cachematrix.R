5e9265237886ada6230cce27a6b3a5834fab402a
liupeibin <liupeibin@outlook.com>
2014-09-20 12:57:56 +0800

    rprog-007 Homework
    rprog-007 Homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matr = matrix()) {
+        inre <- NULL
+        set <- function(y) {
+                matr <<- y
+                inre <<- NULL
+        }
+        get <- function() matr
+        setinverse <- function(i) inre <<- i
+        getinverse <- function() inre
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
-
-
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
+        ir <- x$getinverse()
+        if(!is.null(ir)) {
+                message("getting invserse matrix")
+                return(ir)
+        }
+        data <- x$get()
+        ir <- solve(data, ...)
+        x$setinverse(ir)
+        ir
         ## Return a matrix that is the inverse of 'x'
 }
